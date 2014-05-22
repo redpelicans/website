0d0c9ad99bd54bdca0ed0e8ce6c6d727d4c647b5
tom-ripley <tom-ripley@gmail.com>
2014-05-22 19:46:16 +0200

    wip in services

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -18,41 +18,44 @@ controllers.controller('SlideMenuCtrl', function($scope) {
 });
 
 controllers.controller('ServicesCtrl', function($scope, $routeParams) {
-  $scope.states = {
-    home: {template: 'home', index: 0}
-  , angular: {template: 'angular', index: 1}
-  , d3: {template: 'd3', index: 2}
-  , node: {template: 'node', index: 3}
-  , mongo: {template: 'mongo', index: 4}
-  , consulting: {template: 'consulting', index: 5}
-  };
+  $scope.states = [
+    'home'
+  , 'angular'
+  , 'd3'
+  , 'node'
+  , 'mongo'
+  , 'consulting'
+  ];
   $scope.state = $scope.states[0];
-  $scope.changeState = function(newStateId) {
-    if (_.contains(_.keys($scope.states), newStateId)) {
-      $scope.state = $scope.states[newStateId];
+  $scope.changeState = function(index) {
+    if (_.isNumber(index) && index < $scope.states.length) {
+      $scope.state = $scope.states[index];
+    } else {
+      $scope.state = $scope.states[0];
     }
   }
 
   $scope.carousel = {};
   $scope.carousel.interval = -1;
-  $scope.carousel.slides = _.map($scope.states, function(state) {
-    return { template: state.template };
-  });
+  $scope.carousel.slides = $scope.states;
+  // $scope.carousel.slides = _.map($scope.states, function(state) {
+  //   return { template: state.template };
+  // });
 
-  $scope.carousel.changeSlide = function(index) {
-    if (_.isNumber(index) && index < $scope.carousel.slides.length) {
-      $scope.carousel.slides[index].active = true;
-    }
-  }
+  // $scope.carousel.changeSlide = function(index) {
+  //   if (_.isNumber(index) && index < $scope.carousel.slides.length) {
+  //     $scope.carousel.slides[index].active = true;
+  //   }
+  // }
 
-  $scope.$watch(
-    'state'
-  , function(newState) {
-      if (newState && _.has(newState, 'index')) {
-        $scope.carousel.changeSlide(newState.index);
-      }
-    }
-  , true);
+  // $scope.$watch(
+  //   'state'
+  // , function(newState) {
+  //     if (newState && _.has(newState, 'index')) {
+  //       $scope.carousel.changeSlide(newState.index);
+  //     }
+  //   }
+  // , true);
 
-  $scope.changeState($routeParams.stateId);
+  $scope.changeState(_.indexOf($scope.states, $routeParams.stateId));
 });
