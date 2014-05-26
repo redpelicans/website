d14a8e0f29c2b481322862742dbbe05617a90286
tom-ripley <tom-ripley@gmail.com>
2014-05-26 10:52:51 +0200

    tidy up directives

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -2,28 +2,13 @@
 
 var directives = angular.module('rpApp.directives', []);
 
-directives.directive('scrollPosition', function($window) {
-  return {
-    scope: {
-      scroll: '=scrollPosition'
-    }
-  , link: function($scope) {
-      var window = angular.element($window)
-        , handler = function() { $scope.scroll = window.scrollTop(); };
-      
-      window.on('scroll', $scope.$apply.bind($scope, handler));
-      handler();
-    }
-  };
-});
-
 directives.directive('rpLine', function() {
   return {
     restrict: 'E'
   , replace: true
   , scope: {}
   , transclude: true
-  , templateUrl: 'views/line.html'
+  , templateUrl: 'views/directives/line.html'
   , link: function($scope, $element, attrs) {
       $scope.overrides = attrs.overrides;
     }
