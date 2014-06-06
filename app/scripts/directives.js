eaa9f41896a6611d6922b4d786d526fdc46beba9
tom-ripley <nicolas.briemant@gmail.com>
2014-06-06 17:27:42 +0200

    massive changes !

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -2,19 +2,6 @@
 
 var directives = angular.module('rpApp.directives', []);
 
-directives.directive('rpLine', function() {
-  return {
-    restrict: 'E'
-  , replace: true
-  , scope: {}
-  , transclude: true
-  , templateUrl: 'views/directives/line.html'
-  , link: function($scope, $element, attrs) {
-      $scope.overrides = attrs.overrides;
-    }
-  };
-});
-
 directives.directive('rpHidden', function($window) {
   return {
     restrict: 'A'
