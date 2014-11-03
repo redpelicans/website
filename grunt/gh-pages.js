22c7c3b36616c1e7951ca0df6b8e407ec8e1f340
Nicolas Briemant <nicolas.briemant@gmail.com>
2014-11-03 16:40:39 +0100

    use ssh repo

--- grunt/gh-pages.js
+++ grunt/gh-pages.js
@@ -4,7 +4,7 @@ module.exports = {
   dist: {
     options: {
       base: 'dist'
-    , repo: 'https://github.com/redpelicans/redpelicans.github.io.git'
+    , repo: 'git@github.com:redpelicans/redpelicans.github.io.git'
     , branch: 'master'
     , message: 'deployed dist of https://github.com/redpelicans/website.git'
     }
